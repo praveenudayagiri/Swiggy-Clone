45994d08d89a83f9c021adec97d5615b40e7e080
praveenudayagiri <praveenudayagiri724@gmail.com>
2025-07-18 20:17:28 +0530

    Error route added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,23 @@ import "./index.css";
 import App from "./components/App";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/about", element: <About /> },
-  { path: "/contact", element: <Contact /> }
+  { 
+    path: "/", 
+    element: <App />,
+    errorElement: <Error/>, 
+  },
+  { 
+    path: "/about", 
+    element: <About /> 
+  },
+  { 
+    path: "/contact", 
+    element: <Contact /> 
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
